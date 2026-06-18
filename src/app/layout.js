a8a78099618fedc63ca17cec99cffc09bf9009e5
ankan20 <adas34965@gmail.com>
2026-06-19 00:20:41 +0530

    FIX:Toaster posion changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
       {children}
-      <Toaster richColors position="top-right" />
+      <Toaster richColors position="top-center" />
       </body>
     </html>
   );
