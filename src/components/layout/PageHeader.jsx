746b9fb9afe0a7599fef5b98102efd7db1aebf6e
ankan20 <adas34965@gmail.com>
2026-07-23 01:40:07 +0530

    fix(PageHeader): improve breadcrumb styling for better responsiveness

--- src/components/layout/PageHeader.jsx
+++ src/components/layout/PageHeader.jsx
@@ -17,7 +17,7 @@ export default function PageHeader({ actions = [] }) {
   return (
     <div className="shrink-0 w-full flex flex-col sm:flex-row sm:items-center justify-between px-2 pr-3 py-1 border-b border-[#c4d1df] bg-gradient-to-b from-[#f4f4f4] to-[#e3e3e3] gap-1 ">
       {/* LEFT → BREADCRUMB */}
-      <div className="text-[13px] font-semibold leading-none whitespace-nowrap">
+      <div className="text-[13px] font-semibold leading-none sm:whitespace-nowrap min-w-0 break-words">
         <span>Modules : </span>
 
         {breadcrumbs.map((crumb, index) => (
